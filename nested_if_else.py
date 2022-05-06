884d6a5b898c2492d53090c33708926c15352d10
mobinDahal <dahalmobin3@gmail.com>
2022-05-06 08:59:07 +0545

    nested if else

--- nested_if_else.py
+++ nested_if_else.py
@@ -15,20 +15,56 @@
 #         print("z is greater than y")
 
 #@@@@@@@@@@@@@@@@@@@@@ CLASSWORD @@@@@@@@@@@@@@@@@@@@@#
-username = "admin"
-password = "12345"
-if username == "admin":
-    if password == "12345":
-        print("you are logged in")
+# username = "admin"
+# password = "12345"
+# if username == "admin":
+#     if password == "12345":
+#         print("you are logged in")
+#
+#     else:
+#         print("Incorrect password")
+#
+# else:
+#     if password=="12345":
+#         if username=="admin":
+#             print("you are logged in")
+#         else:
+#             print("incorrect username")
+#     else:
+#         print("incorrect username and password")
 
-    else:
-        print("Incorrect password")
 
-else:
-    if password=="12345":
-        if username=="admin":
-            print("you are logged in")
-        else:
-            print("incorrect username")
-    else:
-        print("incorrect username and password")
+ # even and odd number
+# num=12
+# if num%2==0:
+#     print("enen")
+# else:
+#     print("odd")
+
+
+# num=15
+# if num%3 and num%5 == 0:
+#     print("it is the multiple of 3 and 5")
+# else:
+#     print("not the multiple")
+
+# num=12
+# if num%3 == 0:
+#     if num%5 == 0:
+#         print("it is the multiple of 3 and 5")
+#     else:
+#         print("not the multiple of 5")
+# else:
+#     if num % 5 == 0:
+#         if num % 3 == 0:
+#             print("it is the multiple of 3 and 5")
+#         else:
+#             print("not the multiple of 3")
+#     else:
+#         print("Not is the multiple of 3 and 5")
+
+
+x,*y,z= 19,34,23,5,23,11
+print(x)
+print(y)
+print(z)
