6df22ff30d900edea44ba6d0aabdb24336c2eedf
mobinDahal <dahalmobin3@gmail.com>
2022-05-08 21:00:03 +0545

    assignment and classwork

---
+++ loop1.py
@@ -0,0 +1,80 @@
+# While loop: indefinite iteration
+# example
+
+# x=1
+# while x<10:
+#     print(x,end=" ")
+#     x +=1
+# a =1
+# x = int(input("Enter number of times: "))
+# while a<x:
+#     print("Mobin Dahal")
+#     a +=1
+
+# x=1
+# even=0
+# odd=0
+# a =int(input("Enter number: "))
+# while x<a:
+#     if x%2==0:
+#         even+=x
+#
+#     else:
+#         odd+=x
+#
+#     x+=1
+#
+# print(even)
+# print(odd)
+
+# x =0
+# while x< len(data):
+#     print(data[x])
+#     x+=1
+#
+
+
+#for loop
+#example:
+# data=["ram","sita","gita","hari","mobin"]
+# for value in data:
+#     print(value)
+# data=[["ram","sita"],["gita","hari]","mobin"]]
+# for names in data:
+#     for name in names:
+#         print(name)
+
+# for x in range(10):
+#     print(x)
+
+# wap to enter number of students and print their names
+#five subject marks and print total marks and average marks
+#of each student division wise
+# num=1
+# given=int(input("Enter how many students you want to print: "))
+# while num<given:
+#     name = input("Enter a name: ")
+#
+#     print(f"Name is {num}", name)
+#     science = int(input("Enter marks of science: "))
+#     nepali = int(input("Enter marks of nepali: "))
+#     social = int(input("Enter marks of social: "))
+#     maths = int(input("Enter marks of maths: "))
+#     english = int(input("Enter marks of english: "))
+#     t = science + social + nepali + maths + english
+#     a= t/5
+#     if a < 35:
+#         print("you are fail")
+#     elif a >= 35 and a < 45:
+#         print("You got third division")
+#     elif a >= 45 and a < 60:
+#         print("You got second division")
+#     elif a >= 60 and a < 75:
+#         print("You got first division")
+#     elif a >= 75 and a <= 100:
+#         print("Wow, congralutation you got distinction")
+#
+#     print("Total marks is: ", t)
+#     print("percentage is: ", (t / 5), "%")
+#     num+=1
+
