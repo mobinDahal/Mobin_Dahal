820e088a27ef43abdc6ff492d62483dc8a9e9c03
mobinDahal <dahalmobin3@gmail.com>
2022-05-15 08:04:48 +0545

    zip and problem related to function

--- function2.py
+++ function2.py
@@ -12,12 +12,21 @@
 # repeat(name,times)
 
 # classwork2
-num1=int(input("enter first number:"))
-num2 = int(input("enter second number: "))
-def all(num1,num2):
-    add= num1+num2
-    sub = num1-num2
-    multiply = num1*num2
-    division= num1/num2
-    return add,sub,multiply,division
-print(all(num1,num2))
+# num1=int(input("enter first number:"))
+# num2 = int(input("enter second number: "))
+# def all(num1,num2):
+#     add= num1+num2
+#     sub = num1-num2
+#     multiply = num1*num2
+#     division= num1/num2
+#     return add,sub,multiply,division
+# print(all(num1,num2))
+
+# print(dir(__builtins__))
+data1 = ("Mobin","Samir","Abishek",)
+data2 = ("Dahal","Khanal","khadka")
+# num =(2,3,23,1)
+d1 = zip(data1,data2,)
+
+print(dict(d1))
+# print(len(d1))
