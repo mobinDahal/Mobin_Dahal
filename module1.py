e1e5f998b2725220a1f8aa6d0b954f0312e61987
mobinDahal <dahalmobin3@gmail.com>
2022-05-16 23:16:13 +0545

    different modules

--- module1.py
+++ module1.py
@@ -18,3 +18,21 @@ import example1
 # print(__name__)
 
 # Homework = import os ra import sys
+# import os
+# print(dir(os))
+# print(os.getcwd())
+# print(os.listdir("C://"))
+# os.rename("main.py","starting.py")
+# os.rename("starting.py","main.py")
+# os.mkdir("sample.py")
+# os.rmdir("D://Mobin_Dahal/Projects/sample.py")
+# print(os.name)
+
+
+import sys
+# print(sys.platform)
+# print(sys.copyright)
+# print(sys.version)
+# sys.stdout.write("My name is Mobin Dahla\n")
+# sys.stderr.write("I am supporting Balen\n")
+print(sys.getsizeof("My name is Mobin Dahal"))
