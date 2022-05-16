80c6d8d7840a496b3fc29245560ec789958f57b3
mobinDahal <dahalmobin3@gmail.com>
2022-05-16 08:03:12 +0545

    classwork on modules

---
+++ example1.py
@@ -0,0 +1,9 @@
+def test():
+    return ("Hello World")
+# a = 2
+# x = "mobin"
+# print(__name__)
+
+if __name__=='__main__':
+    print("my name is mobin")
+
---
+++ module1.py
@@ -0,0 +1,20 @@
+# import example1 as ex
+# print(ex.test())
+
+# from example1 import *
+# print(test())
+#
+# print(a)
+# print(x)
+
+# def run():
+#     from example1 import test
+#     return (test())
+# print(run())
+import example1
+
+
+# print(__file__)
+# print(__name__)
+
+# Homework = import os ra import sys
